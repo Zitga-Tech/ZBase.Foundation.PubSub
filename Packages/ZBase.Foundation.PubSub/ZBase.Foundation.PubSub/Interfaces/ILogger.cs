0853509b683acfe4a3a0e181d9a105a6032e5fff
Grashaar <grashaar@outlook.com>
2023-07-11 10:04:12 +0700

    Rename DISABLE_ZBASE_PUBSUB_DEBUG symbol

--- Packages/ZBase.Foundation.PubSub/ZBase.Foundation.PubSub/Interfaces/ILogger.cs
+++ Packages/ZBase.Foundation.PubSub/ZBase.Foundation.PubSub/Interfaces/ILogger.cs
@@ -4,21 +4,21 @@
     {
         void LogWarning(string message)
         {
-#if (UNITY_EDITOR || DEBUG) && !DISABLE_DEBUG
+#if (UNITY_EDITOR || DEBUG) && !DISABLE_ZBASE_PUBSUB_DEBUG
             UnityEngine.Debug.LogWarning(message);
 #endif
         }
 
         void LogError(string message)
         {
-#if (UNITY_EDITOR || DEBUG) && !DISABLE_DEBUG
+#if (UNITY_EDITOR || DEBUG) && !DISABLE_ZBASE_PUBSUB_DEBUG
             UnityEngine.Debug.LogError(message);
 #endif
         }
 
         void LogException(System.Exception exception)
         {
-#if (UNITY_EDITOR || DEBUG) && !DISABLE_DEBUG
+#if (UNITY_EDITOR || DEBUG) && !DISABLE_ZBASE_PUBSUB_DEBUG
             UnityEngine.Debug.LogException(exception);
 #endif
         }
