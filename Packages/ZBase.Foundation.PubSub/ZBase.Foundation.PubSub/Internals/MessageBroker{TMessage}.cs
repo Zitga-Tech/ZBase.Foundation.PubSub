3f9a24cada7d09a666c7ade081932ad3e70df058
Grashaar <grashaar@outlook.com>
2024-03-23 22:57:55 +0700

    Improve MessageBroker<TMessage>

--- Packages/ZBase.Foundation.PubSub/ZBase.Foundation.PubSub/Internals/MessageBroker{TMessage}.cs
+++ Packages/ZBase.Foundation.PubSub/ZBase.Foundation.PubSub/Internals/MessageBroker{TMessage}.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using ZBase.Collections.Pooled.Generic.Internals;
 using ZBase.Collections.Pooled.Generic.Internals.Unsafe;
 
 using ZCPG = ZBase.Collections.Pooled.Generic;
@@ -29,34 +30,27 @@ namespace ZBase.Foundation.PubSub.Internals
 
         public async UniTask PublishAsync(TMessage message, CancellationToken cancelToken, ILogger logger)
         {
-            var handlerMap = _handlerMap;
-            var ordering = _ordering;
+            var handlersArray = GetAllHandlers(logger);
 
-            ordering.GetUnsafe(out var orderArray, out var orderCount);
-            
-            var orderValueArray = ZCPG.ValueArray<int>.Create(orderCount);
-            orderArray.AsSpan(0, orderCount).CopyTo(orderValueArray.AsSpan(0, orderCount));
-            orderValueArray.GetUnsafe(out orderArray, out orderCount);
+            if (handlersArray.IsValid == false)
+            {
+                return;
+            }
 
 #if !__ZBASE_FOUNDATION_PUBSUB_NO_VALIDATION__
             try
 #endif
             {
-                for (var i = orderCount - 1; i >= 0; i--)
+                handlersArray.GetUnsafe(out var handlers, out var length);
+
+                for (var i = 0; i < length; i++)
                 {
                     if (cancelToken.IsCancellationRequested)
                     {
                         break;
                     }
 
-                    var order = orderArray[i];
-
-                    if (handlerMap.TryGetValue(order, out var handlers) == false || handlers.Count < 1)
-                    {
-                        continue;
-                    }
-
-                    await PublishAsync(handlers, message, cancelToken, TaskArrayPool, logger);
+                    await PublishAsync(handlers[i], message, cancelToken, TaskArrayPool, logger);
                 }
             }
 #if !__ZBASE_FOUNDATION_PUBSUB_NO_VALIDATION__
@@ -67,35 +61,94 @@ namespace ZBase.Foundation.PubSub.Internals
             finally
 #endif
             {
-                orderValueArray.Dispose();
+                Dispose(ref handlersArray);
+            }
+        }
+
+        private ZCPG.ValueArray<ZCPG.ValueArray<IHandler<TMessage>>> GetAllHandlers(ILogger logger)
+        {
+            var handlerMap = _handlerMap;
+            var ordering = _ordering;
+
+            lock (handlerMap)
+            {
+                ordering.GetUnsafe(out var orderArray, out var orderCount);
+
+                var orderValueArray = ZCPG.ValueArray<int>.Create(orderArray, orderCount);
+                orderValueArray.GetUnsafe(out var orders, out orderCount);
+
+                var handlersList = ZCPG.ValueList<ZCPG.ValueArray<IHandler<TMessage>>>.Create(handlerMap.Count);
+
+#if !__ZBASE_FOUNDATION_PUBSUB_NO_VALIDATION__
+                try
+#endif
+                {
+                    for (var i = orderCount - 1; i >= 0; i--)
+                    {
+                        var order = orders[i];
+
+                        if (handlerMap.TryGetValue(order, out var handlers) == false || handlers.Count < 1)
+                        {
+                            continue;
+                        }
+
+                        handlers.GetUnsafeValues(out var handlerArray, out var length);
+                        handlersList.Add(ZCPG.ValueArray<IHandler<TMessage>>.Create(handlerArray, length));
+                    }
+                }
+#if !__ZBASE_FOUNDATION_PUBSUB_NO_VALIDATION__
+                catch (Exception ex)
+                {
+                    logger?.LogException(ex);
+                }
+                finally
+#endif
+                {
+                    orderValueArray.Dispose();
+                }
+
+                return ValueCollectionInternals.ToValueArray(ref handlersList);
             }
         }
 
         private static async UniTask PublishAsync(
-              ZCPG.ArrayDictionary<HandlerId, IHandler<TMessage>> handlers
+              ZCPG.ValueArray<IHandler<TMessage>> handlers
             , TMessage message
             , CancellationToken cancelToken
             , CappedArrayPool<UniTask> taskArrayPool
             , ILogger logger
         )
         {
-            handlers.GetUnsafeValues(out var handlerArray, out var count);
+            if (handlers.IsValid == false || handlers.Length < 1)
+            {
+                return;
+            }
 
-            var handlerValueArray = ZCPG.ValueArray<IHandler<TMessage>>.Create(count);
-            handlerArray.AsSpan(0, count).CopyTo(handlerValueArray.AsSpan(0, count));
-            handlerValueArray.GetUnsafe(out handlerArray, out count);
+            handlers.GetUnsafe(out var items, out var length);
 
-            var tasks = taskArrayPool.Rent(count);
-
+            var tasks = taskArrayPool.Rent(length);
+            
+            for (var i = 0; i < length; i++)
+            {
+#if !__ZBASE_FOUNDATION_PUBSUB_NO_VALIDATION__
+                try
+#endif
+                {
+                    tasks[i] = items[i]?.Handle(message, cancelToken) ?? UniTask.CompletedTask;
+                }
+#if !__ZBASE_FOUNDATION_PUBSUB_NO_VALIDATION__
+                catch (Exception ex)
+                {
+                    tasks[i] = UniTask.CompletedTask;
+                    logger?.LogException(ex);
+                }
+#endif
+            }
+            
 #if !__ZBASE_FOUNDATION_PUBSUB_NO_VALIDATION__
             try
 #endif
             {
-                for (var i = 0; i < count; i++)
-                {
-                    tasks[i] = handlerArray[i].Handle(message, cancelToken);
-                }
-
                 await UniTask.WhenAll(tasks);
             }
 #if !__ZBASE_FOUNDATION_PUBSUB_NO_VALIDATION__
@@ -107,7 +160,6 @@ namespace ZBase.Foundation.PubSub.Internals
 #endif
             {
                 taskArrayPool.Return(tasks);
-                handlerValueArray.Dispose();
             }
         }
 
@@ -144,17 +196,48 @@ namespace ZBase.Foundation.PubSub.Internals
             }
         }
 
+        private static void Dispose(ref ZCPG.ValueArray<ZCPG.ValueArray<IHandler<TMessage>>> handlersList)
+        {
+            if (handlersList.IsValid == false)
+            {
+                return;
+            }
+
+            handlersList.GetUnsafe(out var handlersArray, out var length);
+
+            var span = handlersArray.AsSpan(0, length);
+
+            for (var i = 0; i < length; i++)
+            {
+                ref var array = ref span[i];
+
+                if (array.IsValid)
+                {
+                    array.Dispose();
+                }
+
+                array = default;
+            }
+
+            handlersList.Dispose();
+            handlersList = default;
+        }
+
         public sealed override void Dispose()
         {
             var handlerMap = _handlerMap;
+            var ordering = _ordering;
 
-            foreach (var kvp in handlerMap)
+            lock (handlerMap)
             {
-                kvp.Value?.Dispose();
-            }
+                foreach (var kvp in handlerMap)
+                {
+                    kvp.Value?.Dispose();
+                }
 
-            handlerMap.Clear();
-            _ordering.Clear();
+                handlerMap.Clear();
+                ordering.Dispose();
+            }
         }
 
         /// <summary>
@@ -169,9 +252,8 @@ namespace ZBase.Foundation.PubSub.Internals
             {
                 ordering.GetUnsafe(out var orderArray, out var orderCount);
 
-                var orderValueArray = ZCPG.ValueArray<int>.Create(orderCount);
-                orderArray.AsSpan(0, orderCount).CopyTo(orderValueArray.AsSpan(0, orderCount));
-                orderValueArray.GetUnsafe(out orderArray, out orderCount);
+                var orderValueArray = ZCPG.ValueArray<int>.Create(orderArray, orderCount);
+                orderValueArray.GetUnsafe(out var orders, out orderCount);
 
 #if !__ZBASE_FOUNDATION_PUBSUB_NO_VALIDATION__
                 try
@@ -179,7 +261,7 @@ namespace ZBase.Foundation.PubSub.Internals
                 {
                     for (var i = orderCount - 1; i >= 0; i--)
                     {
-                        var order = orderArray[i];
+                        var order = orders[i];
 
                         if (handlerMap.TryGetValue(order, out var handlers) == false)
                         {
